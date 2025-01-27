2b00c06c0125d68be4b2214229f0a4d38adc2777
Mahesh8188 <ddbambhava8182@gmail.com>
2025-01-27 08:32:29 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -31,35 +31,64 @@ async def get_files_db_size():
     return (await mydb.command("dbstats"))['dataSize']
     
 async def save_file(media):
-    """Save file in database"""
+    """Save file in the database."""
+    
+    file_id = unpack_new_file_id(media.file_id)
+    file_name = clean_file_name(media.file_name)
+    
+    file = {
+        'file_id': file_id,
+        'file_name': file_name,
+        'file_size': media.file_size,
+        'caption': media.caption.html if media.caption else None
+    }
 
-    # TODO: Find better way to get same file_id for same media to avoid duplicates
-    file_id, file_ref = unpack_new_file_id(media.file_id)
-    file_name = re.sub(r"(_|\-|\.|\+)", " ", str(media.file_name))
+    if is_file_already_saved(file_id, file_name):
+        return False, 0
+
+    collection_to_use = col if not MULTIPLE_DATABASE else select_collection_based_on_size(file)
+
     try:
-        file = Media(
-            file_id=file_id,
-            file_ref=file_ref,
-            file_name=file_name,
-            file_size=media.file_size,
-            mime_type=media.mime_type,
-            caption=media.caption.html if media.caption else None,
-            file_type=media.mime_type.split('/')[0]
-        )
-    except ValidationError:
-        print('Error occurred while saving file in database')
-        return 'err'
-    else:
-        try:
-            await file.commit()
-        except DuplicateKeyError:      
-            print(f'{getattr(media, "file_name", "NO_FILE")} is already saved in database') 
-            return 'dup'
-        else:
-            print(f'{getattr(media, "file_name", "NO_FILE")} is saved to database')
-            return 'suc'
+        collection_to_use.insert_one(file)
+        print(f"{file_name} is successfully saved.")
+        return True, 1
+    except DuplicateKeyError:
+        print(f"{file_name} is already saved.")
+        return False, 0
 
-async def get_search_results(query, max_results=MAX_BTN, offset=0, lang=None):
+def clean_file_name(file_name):
+    """Clean and format the file name."""
+    file_name = re.sub(r"(_|\-|\.|\+)", " ", str(file_name)) 
+    unwanted_chars = ['[', ']', '(', ')', '{', '}']
+    
+    for char in unwanted_chars:
+        file_name = file_name.replace(char, '')
+        
+    return ' '.join(filter(lambda x: not x.startswith('@') and not x.startswith('http') and not x.startswith('www.') and not x.startswith('t.me'), file_name.split()))
+
+def is_file_already_saved(file_id, file_name):
+    """Check if the file is already saved in either collection."""
+    found1 = {'file_name': file_name}
+    found = {'file_id': file_id}
+
+    for collection in [col, sec_col]:
+        if collection.find_one(found1) or collection.find_one(found):
+            print(f"{file_name} is already saved.")
+            return True
+            
+    return False
+
+def select_collection_based_on_size(file):
+    """Select the appropriate collection based on database size."""
+    result = db.command('dbstats')
+    
+    if result['dataSize'] > 503316480:
+        return sec_col
+    return col
+
+async def get_search_results(chat_id, query, file_type=None, max_results=10, offset=0, filter=False):
+    """For given query return (results, next_offset)"""
+    
     query = query.strip()
     if not query:
         raw_pattern = '.'
@@ -72,50 +101,60 @@ async def get_search_results(query, max_results=MAX_BTN, offset=0, lang=None):
     except:
         regex = query
     filter = {'file_name': regex}
-    cursor = Media.find(filter)
-    cursor.sort('$natural', -1)
-    if lang:
-        lang_files = [file async for file in cursor if lang in file.file_name.lower()]
-        files = lang_files[offset:][:max_results]
-        total_results = len(lang_files)
-        next_offset = offset + max_results
-        if next_offset >= total_results:
-            next_offset = ''
-        return files, next_offset, total_results
-    cursor.skip(offset).limit(max_results)
-    files = await cursor.to_list(length=max_results)
-    total_results = await Media.count_documents(filter)
-    next_offset = offset + max_results
-    if next_offset >= total_results:
-        next_offset = ''       
+    files = []
+    if MULTIPLE_DATABASE:
+        cursor1 = col.find(filter).sort('$natural', -1).skip(offset).limit(max_results)
+        cursor2 = sec_col.find(filter).sort('$natural', -1).skip(offset).limit(max_results)
+        
+        for file in cursor1:
+            files.append(file)
+        for file in cursor2:
+            files.append(file)
+    else:
+        cursor = col.find(filter).sort('$natural', -1).skip(offset).limit(max_results)
+        
+        for file in cursor:
+            files.append(file)
+
+    total_results = col.count_documents(filter) if not MULTIPLE_DATABASE else (col.count_documents(filter) + sec_col.count_documents(filter))
+    next_offset = "" if (offset + max_results) >= total_results else (offset + max_results)
+
     return files, next_offset, total_results
-    
-async def get_bad_files(query, file_type=None, offset=0, filter=False):
+
+async def get_bad_files(query, file_type=None, use_filter=False):
+    """For given query return (results, next_offset)"""
     query = query.strip()
+    
     if not query:
         raw_pattern = '.'
     elif ' ' not in query:
-        raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
+        raw_pattern = rf'(\b|[.+-_]){query}(\b|[.+-_])'
     else:
-        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_]')
+        raw_pattern = query.replace(' ', r'.*[s.+-_]')
+    
     try:
         regex = re.compile(raw_pattern, flags=re.IGNORECASE)
-    except:
-        return []
-    filter = {'file_name': regex}
-    if file_type:
-        filter['file_type'] = file_type
-    total_results = await Media.count_documents(filter)
-    cursor = Media.find(filter)
-    cursor.sort('$natural', -1)
-    files = await cursor.to_list(length=total_results)
+    except re.error:
+        return [], 0
+
+    filter_criteria = {'file_name': regex}
+    if USE_CAPTION_FILTER:
+        filter_criteria = {'$or': [filter_criteria, {'caption': regex}]}
+
+    def count_documents(collection):
+        return collection.count_documents(filter_criteria)
+
+    total_results = (count_documents(col) + count_documents(sec_col) if MULTIPLE_DATABASE else count_documents(col))
+
+    def find_documents(collection):
+        return list(collection.find(filter_criteria))
+
+    files = (find_documents(col) + find_documents(sec_col) if MULTIPLE_DATABASE else find_documents(col))
+
     return files, total_results
-    
+
 async def get_file_details(query):
-    filter = {'file_id': query}
-    cursor = Media.find(filter)
-    filedetails = await cursor.to_list(length=1)
-    return filedetails
+    return col.find_one({'file_id': query}) or sec_col.find_one({'file_id': query})
 
 def encode_file_id(s: bytes) -> str:
     r = b""
@@ -129,12 +168,9 @@ def encode_file_id(s: bytes) -> str:
                 n = 0
             r += bytes([i])
     return base64.urlsafe_b64encode(r).decode().rstrip("=")
-
-def encode_file_ref(file_ref: bytes) -> str:
-    return base64.urlsafe_b64encode(file_ref).decode().rstrip("=")
-
+    
 def unpack_new_file_id(new_file_id):
-    """Return file_id, file_ref"""
+    """Return file_id"""
     decoded = FileId.decode(new_file_id)
     file_id = encode_file_id(
         pack(
@@ -145,6 +181,7 @@ def unpack_new_file_id(new_file_id):
             decoded.access_hash
         )
     )
-    file_ref = encode_file_ref(decoded.file_reference)
-    return file_id, file_ref
+    return file_id
+    
+
     
